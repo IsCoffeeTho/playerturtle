2c123ddd70db93004e0cc901e91d2163fa6adf25
Aaron Menadue <aaron.menadue@gmail.com>
2025-07-18 19:51:38 +0930

    update to 1.21.8

--- src/client/java/org/cwyl/PlayerturtleClientMod.java
+++ src/client/java/org/cwyl/PlayerturtleClientMod.java
@@ -2,9 +2,17 @@ package org.cwyl;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PlayerturtleClientMod implements ClientModInitializer {
+	
+	public static final String MOD_ID = "playerturtle";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	
 	@Override
 	public void onInitializeClient() {
 		
+		LOGGER.info("Starting TCP Server");
 	}
 }
