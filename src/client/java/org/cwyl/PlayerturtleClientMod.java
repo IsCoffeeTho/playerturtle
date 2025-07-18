b756b14b1e1c51be828e47f78498ba5a48a3d5f8
Aaron Menadue <aaron.menadue@gmail.com>
2025-07-18 19:10:32 +0930

    changed files to reflect project

---
+++ src/client/java/org/cwyl/PlayerturtleClientMod.java
@@ -0,0 +1,10 @@
+package org.cwyl;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlayerturtleClientMod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
